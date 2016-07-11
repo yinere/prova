4d1db7305ec9dbe75427ee2f4d7ee800f2d7f1fd
Yinere <yinere@MacBook-Pro-di-Juan.local>
2016-07-11 16:02:27 +0200

    perchè no

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        int i;
+
+        if(true)
+              i = 0;
 	// write your code here
     }
 }
